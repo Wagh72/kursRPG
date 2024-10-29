cc9277e949fe9dc0e85a98afa816ecbb5bc1c965
Andrii <smlkr1294@gmail.com>
2024-10-29 22:18:15 +0200

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,117 +1,170 @@
+п»ї#include <SFML/Graphics.hpp>
+#include <vector>
+#include <sstream>
 #include <iostream>
-#include "SFML/Graphics.hpp"
 
-using namespace std;
-using namespace sf;
-class TileMap : public Drawable, public Transformable
-{
-public:
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
 
-    Texture m_tileset;
-    VertexArray m_vertices;
-    string tileset = "src/textures/GrassAndTrails.png";
-    //функція для завантажування тайлів і текстури для них
-    bool load(const string& tileset, Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+const int TILE_SIZE = 32;
+
+const int LAYER_WIDTH = 16;
+const int LAYER_HEIGHT = 16;
+
+int layer1[LAYER_HEIGHT][LAYER_WIDTH] =
     {
-        if (!m_tileset.loadFromFile(tileset))
-            return false;
-        //робимо "основу" тайлів які будуть зроблені з двух трикутників, тому і 6 вершин
-        m_vertices.setPrimitiveType(sf::Triangles);
-        m_vertices.resize(width * height * 6);
+        {5, 6, 7, 9, 9, 1, 1, 1, 2, 3, 4, 4, 4, 4, 4, 4},
+        {5, 6, 7, 17, 17, 9, 9, 1, 1, 2, 3, 4, 5, 6, 7, 12},
+        {5, 5, 6, 7, 25, 17, 17, 29, 30, 31, 2, 5, 13, 14, 15, 20},
+        {13, 5, 5, 6, 7, 25, 25, 29, 30, 31, 1, 13, 21, 22, 23, 2},
+        {21, 13, 5, 5, 5, 6, 7, 29, 30, 31, 31, 21, 21, 22, 23, 1},
+        {17, 21, 13, 13, 13, 14, 15, 1, 29, 30, 31, 31, 22, 23, 4, 1},
+        {25, 25, 21, 21, 21, 22, 23, 9, 9, 29, 30, 31, 1, 1, 1, 2},
+        {25, 1, 9, 9, 17, 17, 17, 17, 17, 17, 17, 9, 9, 9, 1, 1},
+        {37, 38, 39, 40, 55, 56, 37, 61, 62, 39, 40, 37, 37, 38, 39, 40},
+        {45, 46, 47, 48, 47, 48, 45, 45, 46, 47, 33, 34, 45, 46, 47, 48},
+        {26, 25, 25, 26, 27, 28, 5, 6, 7, 7, 41, 42, 29, 30, 31, 25},
+        {27, 25, 26, 27, 5, 6, 7, 7, 7, 15, 49, 50, 29, 29, 30, 31},
+        {27, 28, 5, 6, 7, 7, 7, 15, 15, 23, 57, 58, 12, 29, 30, 31},
+        {27, 5, 6, 7, 7, 15, 15, 23, 23, 20, 12, 12, 29, 30, 31, 31},
+        {26, 13, 14, 15, 15, 23, 23, 28, 28, 29, 30, 31, 31, 31, 31, 10},
+        {9, 21, 22, 23, 23, 28, 25, 26, 27, 28, 28, 28, 28, 28, 17, 18}};
 
-        //у цих двох циклах ми заповнюємо тайли проходячись по ним
-        for (unsigned int i = 0; i < width; ++i) {
-            for (unsigned int j = 0; j < height; ++j) {
-                int tileNumber = tiles[i + j * width];
-                int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
-                int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+int layer2[LAYER_HEIGHT][LAYER_WIDTH] =
+    {
+        {0, 0, 0, 0, 1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 9, 10, 11, 12, 13, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 17, 18, 19, 20, 21, 1, 2, 3, 4, 5, 0, 0},
+        {1, 2, 3, 4, 25, 26, 27, 28, 29, 9, 10, 11, 12, 13, 0, 0},
+        {9, 10, 11, 12, 33, 34, 35, 36, 37, 17, 18, 19, 20, 21, 43, 44},
+        {17, 18, 19, 20, 21, 0, 0, 0, 0, 25, 26, 27, 28, 29, 51, 52},
+        {25, 26, 27, 28, 29, 0, 50, 0, 0, 33, 34, 35, 36, 37, 0, 0},
+        {33, 34, 35, 36, 37, 6, 0, 0, 0, 0, 0, 0, 49, 0, 0, 0},
+};
 
-                Vertex* triangles = &m_vertices[(i + j * width) * 6];
+int layer3[LAYER_HEIGHT][LAYER_WIDTH] =
+    {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 10, 11, 12, 13},
+        {0, 1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 17, 18, 19, 20, 21},
+        {0, 9, 10, 11, 12, 13, 0, 0, 0, 0, 0, 25, 26, 27, 28, 29},
+        {0, 17, 18, 19, 20, 21, 0, 0, 0, 0, 0, 33, 34, 35, 36, 37},
+        {0, 25, 26, 27, 28, 29, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 33, 34, 35, 36, 37, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-                triangles[0].position = Vector2f(i * tileSize.x, j * tileSize.y);
-                triangles[1].position = Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-                triangles[2].position = Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
-                triangles[3].position = Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
-                triangles[4].position = Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-                triangles[5].position = Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+bool loadTexture(sf::Texture &texture, const std::string &path)
+{
+    return texture.loadFromFile(path);
+}
 
-                triangles[0].texCoords = Vector2f(tu * tileSize.x, tv * tileSize.y);
-                triangles[1].texCoords = Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
-                triangles[2].texCoords = Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
-                triangles[3].texCoords = Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
-                triangles[4].texCoords = Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
-                triangles[5].texCoords = Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+void drawMap(sf::RenderWindow &window, const sf::Texture &texture1, const sf::Texture &texture2, const sf::Texture &texture3)
+{
+    sf::Sprite sprite;
+
+    const int ACTUAL_TILE_SIZE = 32; // 256 / 8 = 32 (256x256 .png = 8(32x32) - tiles)
+    const int TEXTURE_ATLAS_WIDTH = 256;
+
+    sprite.setTexture(texture1);
+    for (int y = 0; y < LAYER_HEIGHT; ++y)
+    {
+        for (int x = 0; x < LAYER_WIDTH; ++x)
+        {
+            int tileNumber = layer1[y][x];
+            if (tileNumber > 0)
+            {
+                sprite.setTextureRect(sf::IntRect((tileNumber - 1) % (TEXTURE_ATLAS_WIDTH / ACTUAL_TILE_SIZE) * ACTUAL_TILE_SIZE,
+                                                  (tileNumber - 1) / (TEXTURE_ATLAS_WIDTH / ACTUAL_TILE_SIZE) * ACTUAL_TILE_SIZE,
+                                                  ACTUAL_TILE_SIZE, ACTUAL_TILE_SIZE));
+                sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+                window.draw(sprite);
             }
         }
-        return true;
     }
 
-private:
-    //Рендер тайлів
-    virtual void draw(RenderTarget& target, RenderStates states)  const
+    sprite.setTexture(texture2);
+    for (int y = 0; y < LAYER_HEIGHT; ++y)
     {
-        states.transform *= getTransform();
-        states.texture = &m_tileset;
-        target.draw(m_vertices, states);
+        for (int x = 0; x < LAYER_WIDTH; ++x)
+        {
+            int tileNumber = layer2[y][x];
+            if (tileNumber > 0)
+            {
+                sprite.setTextureRect(sf::IntRect((tileNumber - 1) % (TEXTURE_ATLAS_WIDTH / ACTUAL_TILE_SIZE) * ACTUAL_TILE_SIZE,
+                                                  (tileNumber - 1) / (TEXTURE_ATLAS_WIDTH / ACTUAL_TILE_SIZE) * ACTUAL_TILE_SIZE,
+                                                  ACTUAL_TILE_SIZE, ACTUAL_TILE_SIZE));
+                sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+                window.draw(sprite);
+            }
+        }
     }
 
-};
+    sprite.setTexture(texture3);
+    for (int y = 0; y < LAYER_HEIGHT; ++y)
+    {
+        for (int x = 0; x < LAYER_WIDTH; ++x)
+        {
+            int tileNumber = layer3[y][x];
+            if (tileNumber > 0)
+            {
+                sprite.setTextureRect(sf::IntRect((tileNumber - 1) % (TEXTURE_ATLAS_WIDTH / ACTUAL_TILE_SIZE) * ACTUAL_TILE_SIZE,
+                                                  (tileNumber - 1) / (TEXTURE_ATLAS_WIDTH / ACTUAL_TILE_SIZE) * ACTUAL_TILE_SIZE,
+                                                  ACTUAL_TILE_SIZE, ACTUAL_TILE_SIZE));
+                sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+                window.draw(sprite);
+            }
+        }
+    }
+}
+
 int main()
 {
-    RenderWindow window(VideoMode(1024, 1024), "Tilemap");
-    //Робимо мапу через масив
-    const int level[] =
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "test");
+
+    sf::Texture texture1, texture2, texture3;
+    if (!loadTexture(texture1, "src/textures/GrassAndTrails.png") ||
+        !loadTexture(texture2, "src/textures/TX_PlantFix.png") ||
+        !loadTexture(texture3, "src/textures/layer3test.png"))
     {
-    1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,35,36,5,6,19,19,2,3,4,1,2,3,4,1,2,5,6,
-    9,6,7,19,19,21,22,19,19,19,11,12,21,22,11,43,44,13,14,19,9,10,11,12,9,10,5,5,6,10,5,6,
-    17,14,15,19,21,19,19,19,19,19,19,19,19,30,19,51,52,21,19,19,6,24,5,6,6,18,19,19,19,5,6,14,
-    25,22,21,23,19,19,19,19,19,6,19,19,19,26,27,35,36,5,19,19,19,32,19,14,14,19,19,21,19,19,19,22,
-    30,13,21,19,19,6,19,19,1,14,6,7,19,2,3,43,44,13,14,19,22,2,19,22,22,2,3,4,1,21,22,4,
-    30,21,11,19,32,6,19,12,6,7,14,15,29,30,11,51,52,21,22,19,5,5,19,12,9,10,23,24,9,10,11,12,
-    17,29,19,20,17,19,19,22,19,15,22,23,29,30,19,51,52,18,19,19,13,19,19,20,5,6,31,32,17,18,19,20,
-    25,29,30,19,19,14,19,22,30,23,23,15,25,26,19,51,52,26,27,21,21,19,19,28,13,14,27,28,25,26,27,28,
-    1,29,29,19,19,22,19,22,19,22,23,23,29,19,19,43,44,2,3,23,23,19,19,19,21,22,3,4,1,5,6,4,
-    9,10,11,19,19,19,19,30,6,7,30,19,9,19,11,35,36,10,11,5,6,31,32,19,19,23,19,24,9,13,14,12,
-    17,19,19,19,19,19,19,6,21,15,19,19,19,19,19,43,44,18,19,13,14,18,19,20,17,19,19,32,17,21,22,20,
-    19,19,23,29,19,19,19,19,22,19,19,19,19,19,27,35,36,26,27,21,22,26,27,28,25,19,19,19,25,26,27,28,
-    1,2,31,32,1,2,31,22,21,23,3,4,1,3,4,43,44,2,3,21,22,2,5,6,6,6,3,23,24,24,5,6,
-    9,10,11,29,30,10,11,12,9,10,11,23,24,3,4,35,36,10,11,12,9,10,13,14,14,14,11,31,32,32,13,14,
-    17,29,29,30,17,21,19,20,21,21,19,31,32,22,4,35,36,5,6,20,17,18,21,22,22,22,19,20,17,18,21,22,
-    25,26,27,29,30,26,23,24,25,26,27,28,29,30,4,35,36,13,14,28,25,26,27,28,25,26,27,28,25,26,27,28,
-    37,38,39,40,40,37,37,38,40,38,39,40,40,38,39,61,62,37,38,37,38,39,37,37,38,37,37,37,37,38,37,38,
-    45,46,45,48,45,45,45,46,45,46,45,45,48,46,45,58,57,45,46,45,46,47,45,45,46,45,45,45,45,46,45,46,
-    17,5,6,7,8,8,8,8,5,6,5,5,6,7,8,35,36,21,22,20,17,18,19,20,17,18,19,20,17,18,19,20,
-    25,5,14,14,14,14,15,16,8,8,8,6,7,8,16,35,36,29,30,28,25,26,27,28,25,26,7,8,25,26,27,28,
-    5,5,19,19,19,14,5,15,16,16,16,6,7,8,24,43,44,2,3,4,1,2,3,4,1,2,15,16,1,2,7,8,
-    13,14,14,19,19,15,5,15,16,16,16,16,16,16,32,35,36,10,11,7,7,8,11,7,8,10,23,24,9,7,8,8,
-    21,22,22,22,14,5,5,19,16,5,24,24,24,24,16,35,36,18,19,15,15,16,19,15,16,18,19,20,17,15,16,16,
-    29,30,30,30,22,19,19,19,14,5,5,32,32,32,24,35,36,26,27,23,23,24,27,23,24,8,8,28,25,23,24,24,
-    19,19,31,32,5,19,19,19,14,5,5,19,19,19,14,35,36,2,3,4,7,8,8,4,15,16,16,4,7,8,3,4,
-    19,31,29,14,5,5,19,5,19,13,13,19,5,19,22,43,44,10,11,12,15,16,16,12,23,24,24,19,19,16,19,12,
-    30,19,19,19,5,13,13,5,19,21,21,19,19,5,5,51,52,18,19,20,23,24,24,20,17,18,19,19,19,23,19,20,
-    30,19,19,19,13,21,21,13,5,30,31,19,19,13,13,51,52,26,27,28,19,19,19,13,21,21,15,29,19,31,19,28,
-    30,19,19,19,21,13,21,19,19,19,19,5,31,19,21,51,52,2,21,13,13,19,19,13,19,23,23,24,19,21,19,22,
-    29,30,19,19,5,21,21,21,21,19,19,19,5,19,31,43,5,10,13,19,19,13,19,19,19,15,19,19,19,19,19,22,
-    17,29,5,19,19,19,19,19,19,21,21,21,13,32,19,51,13,18,13,19,19,13,13,19,19,19,19,19,19,30,19,19,
-    25,26,13,21,21,21,29,19,19,31,32,21,21,24,27,28,25,29,19,18,14,19,19,29,19,19,18,18,18,13,14,28
-    };
-
-
-    TileMap map;
-    if (!map.load("src/textures/GrassAndTrails.png", Vector2u(32, 32), level, 32, 32))
-
+        std::cerr << "Error loading texture" << std::endl;
         return -1;
+    }
+
+    sf::Clock clock;
     while (window.isOpen())
     {
-        Event event;
+        sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == Event::Closed)
+            if (event.type == sf::Event::Closed)
                 window.close();
         }
-        window.clear();
-        window.draw(map);
+
+        window.clear(sf::Color::White);
+        drawMap(window, texture1, texture2, texture3);
         window.display();
+
+        static int frameCount = 0;
+        static float elapsedTime = 0.0f;
+        frameCount++;
+        elapsedTime += clock.restart().asSeconds();
+
+        if (elapsedTime >= 1.0f)
+        {
+            std::stringstream title;
+            title << "FPS: " << frameCount;
+            window.setTitle(title.str());
+            frameCount = 0;
+            elapsedTime = 0.0f;
+        }
     }
+
     return 0;
 }
